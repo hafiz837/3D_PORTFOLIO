9cf60f705791cb2e34bcb0ce6db2264b7c5244aa
hafiz837 <m.umair9292@gmail.com>
2024-08-26 12:57:03 +0500

    fixed error in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -23,7 +23,7 @@ function App() {
         <Experience />
         <Tech />
         <Works />
-        <Feedbacks />
+        {/* <Feedbacks /> */}
         <div className="relative z-10">
           <Contact />
           <div className="absolute inset-0 -z-10">
